bf8831770ca014ce18466f606245008f5f1e73d2
Michael Ojejinmi <mojejinmi@erpinternational.net>
2023-02-05 08:26:43 -0500

    Updating Jenkins/wp-config

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpressuser' );
 define( 'DB_PASSWORD', 'W3lcome123' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'wordpressdbclixxjenkins.cc5iigzknvxd.us-east-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'wordpressdbclixxjenkins.cd7numzl1xfe.us-east-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
